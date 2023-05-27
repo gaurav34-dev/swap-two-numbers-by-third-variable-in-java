c072d915def5871510be1395cbc180017d93ad1f
gbaser54 <68541784+gbaser54@users.noreply.github.com>
2023-05-26 20:56:44 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    int d;
+    int a=3;
+    int b=5;
+    System.out.println("the value of a is "
+      + a);
+     System.out.println("the value of b is "
+      + b);
+    d=a;
+    a=b;
+    b=d;
+    //System.out.println("Hello world!");
+    System.out.println("the value of a is " + a);
+    System.out.println("the value of b is " + b);
+  }
+}
+
+
+//swap two numbers by third variable
